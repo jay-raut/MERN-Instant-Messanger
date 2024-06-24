258b5a6cb997b6cced33008ddcb9294790a649b1
jay-raut <jayraut@my.yorku.ca>
2024-06-24 15:31:52 -0400

    fixed mistake when installing mui-icons

--- front-end/im-frontend/src/components/RegisterForm.js
+++ front-end/im-frontend/src/components/RegisterForm.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import { Box, TextField, Button } from "@mui/material";
 import SnackBar from "./Snackbar";
 
@@ -7,35 +8,48 @@ export default function RegisterForm() {
   const [lastname, setLastName] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [snackBarVisible, setSnackBarVisible] = useState(false);
+  const [snackBarMessage, setSnackBarMessage] = useState("");
 
   const handleFirstName = (event) => {
     setFirstName(event.target.value);
   };
+
   const handleLastName = (event) => {
     setLastName(event.target.value);
   };
+
   const handleUserName = (event) => {
     setUsername(event.target.value);
   };
+
   const handlePassword = (event) => {
     setPassword(event.target.value);
   };
 
-  function registerUser(event) {
+  const registerUser = (event) => {
     event.preventDefault();
-    console.log("Register Data:", { firstname, lastname, username, password });
-  }
+    if (!firstname || !lastname || !username || !password) {
+      setSnackBarVisible(true);
+      
+      return;
+    }
+
+    console.log("Registering user:", { firstname, lastname, username, password });
+  };
 
   return (
+    <>
     <Box component="form" noValidate autoComplete="off" onSubmit={registerUser}>
-      <TextField fullWidth label="First Name" margin="normal" variant="outlined" required onChange={handleFirstName} />
-      <TextField fullWidth label="Last Name" margin="normal" variant="outlined" required onChange={handleLastName} />
-      <TextField fullWidth label="Username" margin="normal" variant="outlined" required onChange={handleUserName} />
-      <TextField fullWidth label="Password" margin="normal" variant="outlined" type="password" required onChange={handlePassword} />
+      <TextField fullWidth label="First Name" margin="normal" variant="outlined" required value={firstname} onChange={handleFirstName} />
+      <TextField fullWidth label="Last Name" margin="normal" variant="outlined" required value={lastname} onChange={handleLastName} />
+      <TextField fullWidth label="Username" margin="normal" variant="outlined" required value={username} onChange={handleUserName} />
+      <TextField fullWidth label="Password" margin="normal" variant="outlined" type="password" required value={password} onChange={handlePassword} />
       <Button type="submit" fullWidth variant="contained" color="primary" sx={{ mt: 2 }}>
         Register
       </Button>
-      <SnackBar></SnackBar>
     </Box>
+    <SnackBar open={snackBarVisible} setOpen={setSnackBarVisible} message={snackBarMessage} />
+    </>
   );
 }
