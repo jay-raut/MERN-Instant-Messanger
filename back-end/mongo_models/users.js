251776f86ea45479a414e11a773fae204afcd53f
jay-raut <jayraut@my.yorku.ca>
2024-06-24 13:10:31 -0400

    added model schemas

--- back-end/mongo_models/users.js
+++ back-end/mongo_models/users.js
@@ -1,2 +1,17 @@
 const mongoose = require("mongoose");
 
+const userSchema = mongoose.Schema(
+  {
+    first_name: { type: String, required: true, trim: true },
+    last_name: { type: String, required: true, trim: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
