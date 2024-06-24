9bfe1078f9adba46afc840dadd3a8d24560e16d3
jay-raut <jayraut@my.yorku.ca>
2024-06-24 17:56:00 -0400

    implemented register function

--- front-end/im-frontend/src/components/LoginForm.js
+++ front-end/im-frontend/src/components/LoginForm.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import { Box, TextField, Button } from '@mui/material';
+import { useState } from 'react';
 
 export default function LoginForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUserName = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  async function loginUser(event){
+    event.preventDefault();
+    
+  }
+
   return (
-    <Box component="form" noValidate autoComplete="off">
+    <Box component="form" noValidate autoComplete="off" onSubmit={loginUser}>
 
       <TextField
         fullWidth
@@ -11,6 +28,7 @@ export default function LoginForm() {
         margin="normal"
         variant="outlined"
         required
+        onChange={handleUserName}
       />
       <TextField
         fullWidth
@@ -19,8 +37,9 @@ export default function LoginForm() {
         variant="outlined"
         type="password"
         required
+        onChange={handlePassword}
       />
-      <Button fullWidth variant="contained" color="primary" sx={{ mt: 2 }}>
+      <Button type='submit' fullWidth variant="contained" color="primary" sx={{ mt: 2 }}>
         Login
       </Button>
     </Box>
--- front-end/im-frontend/src/components/RegisterForm.js
+++ front-end/im-frontend/src/components/RegisterForm.js
@@ -8,6 +8,8 @@ export default function RegisterForm() {
   const [lastname, setLastName] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   const [snackBarVisible, setSnackBarVisible] = useState(false);
   const [snackBarMessage, setSnackBarMessage] = useState("");
 
@@ -27,29 +29,52 @@ export default function RegisterForm() {
     setPassword(event.target.value);
   };
 
-  const registerUser = (event) => {
+  const handleConfirmPassword = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  async function registerUser(event) {
     event.preventDefault();
-    if (!firstname || !lastname || !username || !password) {
+    if (!firstname || !lastname || !username || !password || !confirmPassword) {
       setSnackBarVisible(true);
-      
+      setSnackBarMessage("Fill out all fields");
+      return;
+    }
+    if (password !== confirmPassword) {
+      setSnackBarVisible(true);
+      setSnackBarMessage("Passwords do not match");
       return;
     }
 
-    console.log("Registering user:", { firstname, lastname, username, password });
-  };
+    const response = await fetch("http://localhost:4000/register", {
+      method: "POST",
+      body: JSON.stringify({ firstname, lastname, username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+        setSnackBarVisible(true);
+        setSnackBarMessage("Created user successfully");
+        
+    } else {
+      const error = await response.json();
+      setSnackBarVisible(true);
+      setSnackBarMessage(error.error);
+    }
+  }
 
   return (
     <>
-    <Box component="form" noValidate autoComplete="off" onSubmit={registerUser}>
-      <TextField fullWidth label="First Name" margin="normal" variant="outlined" required value={firstname} onChange={handleFirstName} />
-      <TextField fullWidth label="Last Name" margin="normal" variant="outlined" required value={lastname} onChange={handleLastName} />
-      <TextField fullWidth label="Username" margin="normal" variant="outlined" required value={username} onChange={handleUserName} />
-      <TextField fullWidth label="Password" margin="normal" variant="outlined" type="password" required value={password} onChange={handlePassword} />
-      <Button type="submit" fullWidth variant="contained" color="primary" sx={{ mt: 2 }}>
-        Register
-      </Button>
-    </Box>
-    <SnackBar open={snackBarVisible} setOpen={setSnackBarVisible} message={snackBarMessage} />
+      <Box component="form" noValidate autoComplete="off" onSubmit={registerUser}>
+        <TextField fullWidth label="First Name" margin="normal" variant="outlined" required value={firstname} onChange={handleFirstName} />
+        <TextField fullWidth label="Last Name" margin="normal" variant="outlined" required value={lastname} onChange={handleLastName} />
+        <TextField fullWidth label="Username" margin="normal" variant="outlined" required value={username} onChange={handleUserName} />
+        <TextField fullWidth label="Password" margin="normal" variant="outlined" type="password" required value={password} onChange={handlePassword} />
+        <TextField fullWidth label="Confirm Password" margin="normal" variant="outlined" type="password" required value={confirmPassword} onChange={handleConfirmPassword} />
+        <Button type="submit" fullWidth variant="contained" color="primary" sx={{ mt: 2 }}>
+          Register
+        </Button>
+      </Box>
+      <SnackBar open={snackBarVisible} setOpen={setSnackBarVisible} message={snackBarMessage} />
     </>
   );
 }
