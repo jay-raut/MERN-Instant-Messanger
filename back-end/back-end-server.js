930d851b6f915e776a9e16b5abde3952413bd9e2
jay-raut <jayraut@my.yorku.ca>
2024-06-24 12:28:40 -0400

    setup files

--- back-end/back-end-server.js
+++ back-end/back-end-server.js
@@ -1,7 +1,20 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 
+
+app.use(cors({
+    origin: "http://localhost:3000"
+}));
+
+
+app.post("/register", (req, res) => {
+    
+});
+
+app.post("/login", (req, res) =>{
+
+});
 
 app.listen(4000, console.log("Server started on port 4000"));
 
