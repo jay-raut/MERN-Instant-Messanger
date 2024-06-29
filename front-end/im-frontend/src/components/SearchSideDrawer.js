5a959dbb0c25981fb67d4b0245ddb0e3b5c9d223
jay-raut <jayraut@my.yorku.ca>
2024-06-29 00:13:07 -0400

    Searching ui improvement for no results
    When there are no results found for searching a toast appears. Since searchResults is a useState, a useEffect hook is needed to look at the updated state.

--- front-end/im-frontend/src/components/SearchSideDrawer.js
+++ front-end/im-frontend/src/components/SearchSideDrawer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Drawer, IconButton } from "@mui/material";
 import { Typography, Box } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
@@ -11,13 +11,19 @@ import LoadingUsers from "./LoadingUsers";
 import ListUsersSearch from "./ListUsersSearch";
 export default function SearchSideDrawer() {
   const [isDrawerOpen, setDrawerOpen] = useState(false);
+  const [hasSeached, setHasSearched] = useState(false);
   const [searchWord, setSearchWord] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [loading, setLoading] = useState(false);
   const [snackBarVisible, setSnackBarVisible] = useState(false);
   const [snackBarMessage, setSnackBarMessage] = useState("");
-
-  async function handleSearchButton() {
+  useEffect(() => { //useState is async ??
+    if (searchResult.length === 0 && hasSeached) {
+      setSnackBarMessage("No results founds");
+      setSnackBarVisible(true);
+    }
+  }, [searchResult]);
+  async function HandleSearchButton() {
     if (!searchWord) {
       setSnackBarVisible(true);
       setSnackBarMessage("Please enter something in search");
@@ -36,6 +42,7 @@ export default function SearchSideDrawer() {
 
       setSearchResult(search_results.users);
       setLoading(false);
+      setHasSearched(true);
     } catch (e) {
       console.log(e);
       setSnackBarMessage("Unable to search for users try loading");
@@ -103,18 +110,12 @@ export default function SearchSideDrawer() {
           </Typography>
           <Box sx={{ display: "flex", alignItems: "center", marginTop: "60px", padding: "16px", borderTop: "2px solid grey", borderBottom: "2px solid grey" }}>
             <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Search for people" inputProps={{ "aria-label": "search users" }} value={searchWord} onChange={(e) => setSearchWord(e.target.value)} />
-            <IconButton type="button" sx={{ p: "10px" }} aria-label="search" onClick={handleSearchButton}>
+            <IconButton type="button" sx={{ p: "10px" }} aria-label="search" onClick={HandleSearchButton}>
               <SearchIcon />
             </IconButton>
             <SnackBar open={snackBarVisible} setOpen={setSnackBarVisible} message={snackBarMessage}></SnackBar>
           </Box>
-          {loading ? (
-            <>
-              <LoadingUsers></LoadingUsers>
-            </>
-          ) : (
-            searchResult?.map((user) => <ListUsersSearch key={user.id} user={user} handleClick={null} />)
-          )}
+          {loading ? <LoadingUsers /> : <>{searchResult.length === 0 ? <div></div> : searchResult.map((user) => <ListUsersSearch key={user.id} user={user} handleClick={null} />)}</>}
         </Box>
       </Drawer>
     </>
