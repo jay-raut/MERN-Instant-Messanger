a813fe682cee497fe28c4c1e80db797ca58f011f
jay-raut <jayraut@my.yorku.ca>
2024-06-28 22:00:44 -0400

    search results now clickable

--- front-end/im-frontend/src/components/ListUsersSearch.js
+++ front-end/im-frontend/src/components/ListUsersSearch.js
@@ -1,22 +1,21 @@
 import React from "react";
-import { Box, Typography, Avatar, Stack } from "@mui/material";
+import { Box, Typography, Avatar, Stack, ButtonBase } from "@mui/material";
 
 export default function ListUsersSearch({ user, handleClick }) {
-  const { first_name, last_name, username } = user;
+  const { _id, first_name, last_name, username } = user;
 
   const initials = `${first_name.charAt(0)}`.toUpperCase();
 
   const handleButtonClick = () => {
-    // Handle click action, if needed
-    if (handleClick) {
-      handleClick(user._id);
-    }
+    console.log(_id);
   };
 
   return (
-    <Box sx={{ p: 2, border: "1px solid #ccc", marginTop: 2, borderRadius: "5px", display: "flex", alignItems: "center" }}>
-      <Avatar sx={{ width: 50, height: 50, marginRight: 1 }}>{initials}</Avatar>
-      <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "flex-start", marginLeft: 10 }}>
+    <ButtonBase
+      onClick={handleButtonClick}
+      sx={{ p: 2, border: "1px solid #ccc", marginTop: 2, borderRadius: "5px", width: "100%", textAlign: "left", display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+      <Avatar sx={{ width: 50, height: 50 }}>{initials}</Avatar>
+      <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", marginLeft: "auto", marginRight: "auto" }}>
         <Typography variant="h6" sx={{ marginBottom: 1 }}>
           {first_name} {last_name}
         </Typography>
@@ -24,6 +23,6 @@ export default function ListUsersSearch({ user, handleClick }) {
           Username: {username}
         </Typography>
       </Box>
-    </Box>
+    </ButtonBase>
   );
 }
