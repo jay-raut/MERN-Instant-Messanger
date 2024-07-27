4bc3e7f096c825792712971a0b91f266d1f2aa08
jay-raut <jayraut@my.yorku.ca>
2024-07-26 23:36:02 -0400

    Added error alert for unsuccessful  login.
    Unsuccessful login shows an error on login page.

--- front-end/im-frontend/src/components/HomePageComponents/Forms/LoginForm.js
+++ front-end/im-frontend/src/components/HomePageComponents/Forms/LoginForm.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { Box, TextField, Button } from '@mui/material';
-import { useState } from 'react';
+import React from "react";
+import { Box, TextField, Button } from "@mui/material";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Alert } from "@mui/material";
 export default function LoginForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
   const navigate = useNavigate();
   const handleUserName = (event) => {
     setUsername(event.target.value);
@@ -14,45 +16,35 @@ export default function LoginForm() {
     setPassword(event.target.value);
   };
 
-  async function loginUser(event){
+  async function loginUser(event) {
     event.preventDefault();
-    const response = await fetch("http://localhost:4000/api/user/login",{
+    const response = await fetch("http://localhost:4000/api/user/login", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
-      credentials: 'include'
+      credentials: "include",
     });
-    if (response.ok){
+    if (response.ok) {
       console.log("logged in");
-      navigate('/chats');
-    }else{
-      console.log('could not login');
+      navigate("/chats");
+    } else {
+      console.log("could not login");
+      setError(true);
     }
   }
 
   return (
     <Box component="form" noValidate autoComplete="off" onSubmit={loginUser}>
-
-      <TextField
-        fullWidth
-        label="Username"
-        margin="normal"
-        variant="outlined"
-        required
-        onChange={handleUserName}
-      />
-      <TextField
-        fullWidth
-        label="Password"
-        margin="normal"
-        variant="outlined"
-        type="password"
-        required
-        onChange={handlePassword}
-      />
-      <Button type='submit' fullWidth variant="contained" color="primary" sx={{ mt: 2 }}>
+      <TextField fullWidth label="Username" margin="normal" variant="outlined" required onChange={handleUserName} />
+      <TextField fullWidth label="Password" margin="normal" variant="outlined" type="password" required onChange={handlePassword} />
+      <Button type="submit" fullWidth variant="contained" color="primary" sx={{ mt: 2 }}>
         Login
       </Button>
+      {error && (
+        <Alert variant="filled" severity="error" sx={{ marginTop: 1 }}>
+          Could not login. Username or password incorrect.
+        </Alert>
+      )}
     </Box>
   );
 }
